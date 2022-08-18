a9e990466498863ea47e0d5f9e631f81e51d7f0c
rimedevrem <108761099+rimedevrem@users.noreply.github.com>
2022-08-19 00:21:54 +0300

    Update ReaderTest.java

--- section14/ReaderTest.java
+++ section14/ReaderTest.java
@@ -2,14 +2,14 @@ package part1.section14;
 
 public class ReaderTest {
 	public static void main(String[] args) {
-		Author author1 = new Author("Jack LONDON");
-		Author author2 = new Author("Agatha CHRISTIE");
-		Book book1 = new Book(author1, "MARTIN EDEN", 520, false, 20, BookType.FICTION);
-		Book book2 = new Book(author2, "10 Küçük Zenci", 224, true, 0, BookType.DETECTIVE);
-		Book book3 = new Book(author2, "Doğu Ekspresinde Cinayet", 256, true, 99, BookType.DETECTIVE);
-		Reader reader1 = new Reader("Gamze", 25, 'K', book1);
-		Reader reader2 = new Reader("Afra", 13, 'K', book2);
-		Reader reader3 = new Reader("Selim", 49, 'E', book3);
+		Author author1 = new Author("ZÜLFÜ LİVANELİ");
+		Author author2 = new Author("AZİZ NESİN");
+		Book book1 = new Book(author1, "HUZURSUZLUK", 325, false, 20, BookType.FICTION);
+		Book book2 = new Book(author2, "ZÜBÜK", 654, true, 0, BookType.DETECTIVE);
+		Book book3 = new Book(author2, "AZİZNAME", 256, true, 99, BookType.DETECTIVE);
+		Reader reader1 = new Reader("Z", 25, 'K', book1);
+		Reader reader2 = new Reader("Y", 13, 'K', book2);
+		Reader reader3 = new Reader("X", 49, 'E', book3);
 		System.out.println("current page: " + book1.getCurrentPage());
 		reader1.read(book1);
 		System.out.println("current page: " + book1.getCurrentPage());
